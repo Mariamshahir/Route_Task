000c118d77a07b5326e5f1dfc309de1048bf37a4
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-18 15:18:10 +0300

    Finished search screen.

---
+++ lib/ui/widget/search_article.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchArticle extends StatelessWidget {
+  const SearchArticle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
