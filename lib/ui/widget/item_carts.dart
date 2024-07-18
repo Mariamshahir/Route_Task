ff14067fe5cb066097f14dfbc9d1d9eba5857da3
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-18 15:17:48 +0300

    Finished product cart screen.

---
+++ lib/ui/widget/item_carts.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:mariam/model/products.dart';
+import 'package:mariam/utils/colors_app.dart';
+import 'package:mariam/utils/theme_app.dart';
+
+class ItemCarts extends StatelessWidget {
+  final Products product;
+
+  const ItemCarts({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.4,
+      padding: const EdgeInsets.all(4),
+      margin: const EdgeInsets.symmetric(horizontal: 4),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        border: Border.all(color: AppColor.primary),
+        color: AppColor.white,
+      ),
+      child: GridTile(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Positioned.fill(
+                  child: CachedNetworkImage(
+                    imageUrl: (product.images != null && product.images!.isNotEmpty)
+                        ? product.images!.first
+                        : '',
+                    fit: BoxFit.cover,
+                    placeholder: (_, __) => const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.blue,
+                      ),
+                    ),
+                    errorWidget: (_, __, ___) => const Icon(
+                      Icons.image_not_supported_outlined,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.1,
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColor.shadow,
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                          )
+                        ]),
+                    child: const Icon(
+                      Icons.favorite_border,
+                      color: AppColor.primary,
+                      size: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.title ?? '',
+                    style: AppTheme.text,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    product.description ?? '',
+                    style: AppTheme.text,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "EGP ${product.discountPercentage ?? ''}",
+                        style: AppTheme.text,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.05,
+                      ),
+                      Text(
+                        "${product.price ?? ''} EGP",
+                        style: AppTheme.oldPrice,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "Review (${product.rating ?? ''})",
+                            style: AppTheme.review,
+                          ),
+                          const SizedBox(width: 4),
+                          const Icon(
+                            Icons.star_outlined,
+                            color: AppColor.starIcon,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColor.primary,
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 19,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/widget/product_carts.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mariam/business_logic/cubit/product_cubit.dart';
+import 'package:mariam/model/products.dart';
+import 'package:mariam/ui/widget/item_carts.dart';
+import 'package:mariam/ui/widget/loadingapp.dart';
+
+class ProductsCarts extends StatefulWidget {
+  const ProductsCarts({super.key});
+
+  @override
+  State<ProductsCarts> createState() => _ProductsCartsState();
+}
+
+class _ProductsCartsState extends State<ProductsCarts> {
+  late List<Products> allProduct;
+  late List<Products> searchedForCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<ProductCubit>(context).getAllProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductCubit, ProductState>(builder: (context, state) {
+      if (state is ProductLoaded) {
+        allProduct = (state).product;
+        return buildItemProductCarts();
+      } else {
+        return const LoadingApp();
+      }
+    });
+  }
+
+  Widget buildItemProductCarts() {
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.transparent,
+        child: Column(
+          children: [buildItemCartsLst()],
+        ),
+      ),
+    );
+  }
+
+  Widget buildItemCartsLst() {
+    return GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 10),
+        shrinkWrap: true,
+        physics: const ClampingScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: allProduct.length,
+        itemBuilder: (context, index) {
+          return ItemCarts(
+            product: allProduct[index],
+          );
+        });
+  }
+}
