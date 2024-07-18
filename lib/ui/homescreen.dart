e29c68b5224737c698f037c46f1579b25ef09cce
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-18 20:20:34 +0300

    Finished project.

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:mariam/ui/widget/product_carts.dart';
+import 'package:mariam/ui/widget/search_article.dart';
 import 'package:mariam/utils/assets_app.dart';
 import 'package:mariam/utils/colors_app.dart';
+import 'package:mariam/utils/theme_app.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routeName = "homescreen";
-
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final searchTextController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         leading: Column(
@@ -18,26 +20,48 @@ class HomeScreen extends StatelessWidget {
               builder: (BuildContext context) {
                 return Image.asset(
                   AppAssets.logo,
-                  width: MediaQuery.of(context).size.width * 0.2,
-                  height: MediaQuery.of(context).size.height * 0.03,
-                ); // Using the AssetImage with Image.asset
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                );
               },
             ),
           ],
         ),
+        title: TextField(
+          controller: searchTextController,
+          decoration: InputDecoration(
+            contentPadding: const EdgeInsets.symmetric(vertical: 8.0),
+            // Adjust the padding as needed
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: AppColor.primary),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              borderSide: const BorderSide(color: AppColor.primary),
+            ),
+            prefixIcon:
+                const Icon(Icons.search, size: 30, color: AppColor.primary),
+            hintText: "What do you search for?",
+            hintStyle: AppTheme.search,
+          ),
+          style: AppTheme.search,
+          onTap: () {
+            showSearch(context: context, delegate: SearchArticle());
+          },
+        ),
         actions: [
           IconButton(
-              onPressed: () {
-                //showSearch(context: context, delegate: SearchArticle());
-              },
-              icon: const Icon(
-                Icons.shopping_cart_outlined,
-                color: AppColor.primary,
-                size: 30,
-              ))
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_cart_outlined,
+              color: AppColor.primary,
+              size: 30,
+            ),
+          ),
         ],
       ),
-      body:  const ProductsCarts(),
+      body: const ProductsCarts(),
     );
   }
 }
