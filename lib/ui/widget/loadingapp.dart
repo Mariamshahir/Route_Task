c4fecefad2d84ed594d803a4a541508854d0a32e
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-18 15:12:26 +0300

    Finished loading widget.

---
+++ lib/ui/widget/loadingapp.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoadingApp extends StatelessWidget {
+  const LoadingApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
