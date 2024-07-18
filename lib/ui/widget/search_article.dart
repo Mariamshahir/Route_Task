5d3843fe9e47d2e4ba32c60a93f48bcce65b1a18
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-18 21:07:51 +0300

    Finished search screen.

--- lib/ui/widget/search_article.dart
+++ lib/ui/widget/search_article.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mariam/ui/widget/product_carts.dart';
+import 'package:mariam/utils/colors_app.dart';
 
-class SearchArticle extends StatelessWidget {
-  const SearchArticle({super.key});
+class SearchArticle extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          showResults(context);
+        },
+        icon: const Icon(Icons.search),
+      )
+    ];
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(Icons.close),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const ProductsCarts();
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        iconTheme: IconThemeData(color: AppColor.primary),
+        color: AppColor.white,
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        border: InputBorder.none,
+      ),
+    );
   }
 }
